5131da7da94a43ecaee10ec444de9792d84bee78
黄鑫 <xing.huang08@hand-china.com>
2019-11-16 15:57:42 +0800

    [add]service层动态切换数据源

--- src/main/java/com/example/controller/VideoController.java
+++ src/main/java/com/example/controller/VideoController.java
@@ -1,13 +1,18 @@
 package com.example.controller;
 
+import com.example.entity.User;
 import com.example.entity.Video;
+import com.example.service.IUserService;
 import com.example.service.IVideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * <p>
@@ -24,9 +29,30 @@ public class VideoController {
     @Autowired
     private IVideoService videoService;
 
+    @Autowired
+    private IUserService userService;
+
     @RequestMapping("/query")
     public List<Video> selectAll() {
         return videoService.selectAll();
+    }
+
+    @RequestMapping("/name")
+    public List<String> selectName() {
+        Set<String> name = new HashSet<>();
+
+        List<User> users = userService.selectAll();
+        List<Video> videos = videoService.selectAll();
+
+        for (User u : users) {
+            for (Video v : videos) {
+                if (u.getUserId().equals(v.getUserId())) {
+                    name.add(u.getUserName());
+                }
+            }
+        }
+
+        return new ArrayList<>(name);
     }
 	
 }
